13ee1ed63bd026c5b9bbd779ac48abb807982be3
pooh <cktjdgus45@naver.com>
2022-01-02 23:16:06 +0900

    practice react-router a little

---
+++ src/components/profile.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Profile = (props) => {
+    return (
+        <>
+            <h1>Profile</h1>
+            <button>Go to Home</button>
+        </>
+    )
+}
+
+export default Profile;
