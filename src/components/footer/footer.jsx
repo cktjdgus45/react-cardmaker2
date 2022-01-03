d1bdcab543bc81fc8cedbfbfc8d197bc09df6e54
pooh <cktjdgus45@naver.com>
2022-01-03 17:46:24 +0900

    make OAuth google,gitghub to app

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = (props) => {
+    return (
+        <footer>
+            <h3>Code your Dream</h3>
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ src/components/header/header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = (props) => {
+    return (
+        <header>
+            <h3>Business Card Maker</h3>
+        </header>
+    )
+}
+
+export default Header;
